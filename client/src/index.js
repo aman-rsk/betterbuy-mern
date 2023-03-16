b1919bbd6cf8445bfeec8ea76a492700067ba630
aman-rsk <115797086+aman-rsk@users.noreply.github.com>
2023-03-16 13:36:42 +0530

    Updating all files.

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StoreProvider } from './Store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<StoreProvider>
+			<HelmetProvider>
+				<PayPalScriptProvider deferLoading={true}>
+					<App />
+				</PayPalScriptProvider>
+			</HelmetProvider>
+		</StoreProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
